51f90d6415a6a7d92799a635be59e519cce9af8c
bejo <bejoann@gmail.com>
2018-11-03 09:12:12 +1100

    update code

--- script.js
+++ script.js
@@ -3,12 +3,12 @@
 
 var cardContact = document.getElementById('card-contact');
 var cardsContact = document.getElementById('cards-contact');
-var card3Contact = document.getElementById('card3-contact');
+var card2Contact = document.getElementById('card2-contact');
 var flipContact = document.getElementById('flipContact');
 
 flipContact.addEventListener('click', function() {
     cardsContact.classList.toggle('flipped');
-    card3Contact.classList.toggle('flipped');
+    card2Contact.classList.toggle('flipped');
     cardContact.classList.toggle('flipped');
 }, false);
 
@@ -16,11 +16,38 @@ flipContact.addEventListener('click', function() {
 
 var cardAbout = document.getElementById('card-about');
 var cardsAbout = document.getElementById('cards-about');
+var cards2About = document.getElementById('cards2-about');
 var flipAbout = document.getElementById('flipAbout');
 
 flipAbout.addEventListener('click', function() {
     cardsAbout.classList.toggle('flipped');
+    cards2About.classList.toggle('flipped');
     cardAbout.classList.toggle('flipped');
 }, false);
+
+
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var trigger = document.getElementById('trigger');
+
+// Get the <span> element that closes the modal
+var close = document.getElementsByClassName('close')[0];
+
+
+trigger.addEventListener('click', function () {
+    modal.classList.add('visible');
+});
+
+close.addEventListener('click', function(){
+    modal.classList.remove('visible');
+});
+
+window.addEventListener('click',function(event){
+    if (event.target == modal){
+        modal.classList.remove('visible');
+    }
+});
+
 
 
